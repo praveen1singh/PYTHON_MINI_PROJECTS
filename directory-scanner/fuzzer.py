75d1cf1db6e375fd3303954b674766e09e2abf24
PRAVEEN SINGH NEGI <125494177+praveen1singh@users.noreply.github.com>
2026-06-20 02:49:07 +0530

    Create fuzzer.py

---
+++ directory-scanner/fuzzer.py
@@ -0,0 +1,14 @@
+import requests
+import sys
+
+    
+def loop():
+    for word in sys.stdin:
+        res = requests.get(url=f"https://facebook.com/{word}")
+        if res.status_code == 404:
+            loop()
+        else:
+             data = res.text
+             print(data)
+             print(res.status_code)
+             print(word)
